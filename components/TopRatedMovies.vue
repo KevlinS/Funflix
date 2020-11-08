da688410dfc1fa546700c1db7bf2050b9bba8946
Kevlin <kevlin.susanto@yahoo.com>
2020-11-08 05:21:02 +0100

    TopRated

--- components/TopRatedMovies.vue
+++ components/TopRatedMovies.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="toprated_movies">
-    <h1>TOP RATEDD</h1>
+    <h1>TOP RATED</h1>
     <li v-for="movie in moviesList" :key="movie.id">
       <p>{{ movie.original_title }}</p>
       <img :src="imgURL + movie.poster_path" />
