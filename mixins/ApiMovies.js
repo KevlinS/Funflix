edda01a4cfc6390d92ae5f5f745bed22b8b57ff3
Kevlin <kevlin.susanto@yahoo.com>
2020-11-15 03:01:25 +0100

    heroku

--- mixins/ApiMovies.js
+++ mixins/ApiMovies.js
@@ -1,7 +1,7 @@
 export default {
     methods: {
         getMoviesNowPlaying() {
-            return fetch(process.env.BASE_URL || 'https://api.themoviedb.org/3/movie/now_playing?api_key=9dcb183679039b039c527c347b054639&language=en-US&page=1')
+            return fetch('https://api.themoviedb.org/3/movie/now_playing?api_key=9dcb183679039b039c527c347b054639&language=en-US&page=1')
                 .then(res => res.json())
         },
         getMoviesPopular() {
