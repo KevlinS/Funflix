980e097f49586d9c56f508db1b02c2d0b28d996c
KevlinS <kevlin.susanto@yahoo.com>
2022-03-18 12:26:06 +0100

    search

--- components/Header.vue
+++ components/Header.vue
@@ -16,7 +16,10 @@
     </ul>
    <form class="form-inline my-2 my-lg-0">
       <input class="form-control mr-sm-2" name="searchValue" type="search" placeholder="Search" aria-label="Search" v-model="searchValue">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit" v-on:click="submit()">Search</button>
+      <NuxtLink :to="`/search?searchValue=${searchValue}`" >
+       <button class="btn btn-outline-success my-2 my-sm-0" type="submit" v-on:click="submit()">Search</button>
+      </NuxtLink>
+     
     </form>
   </div>
 </nav>
@@ -37,7 +40,8 @@ export default {
 
   methods: {
       submit(){
-        this.$router.push("/search?searchValue="+this.searchValue);
+        this.$route.push('/search')
+        location.reload();
       }
   },
  
